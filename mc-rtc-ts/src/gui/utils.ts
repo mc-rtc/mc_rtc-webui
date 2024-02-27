2ff6042f012453294fb3719f6be8b5213c8198d3
Pierre Gergondet <pierre.gergondet@gmail.com>
2024-02-27 12:19:45 +0900

    Better handling of PolyhedronConfig
    The only thing not handled is show_vertices

--- mc-rtc-ts/src/gui/utils.ts
+++ mc-rtc-ts/src/gui/utils.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 import { Color } from '../types/Color';
+import { LineConfig, LineStyle } from '../types/LineConfig';
 
 export function makeColor(color: Color) {
   return new THREE.Color(color.r, color.g, color.b);
@@ -11,8 +12,26 @@ export function makeMaterial(color: Color) {
   return new THREE.MeshStandardMaterial({ color: rgb, transparent: color.a != 1.0, opacity: color.a });
 }
 
-export function updateMaterial(material: THREE.MeshStandardMaterial, color: Color) {
+export function updateMaterial(
+  material: THREE.MeshStandardMaterial | THREE.LineBasicMaterial | THREE.LineDashedMaterial,
+  color: Color
+) {
   material.color = makeColor(color);
   material.transparent = color.a != 1.0;
   material.opacity = color.a;
+}
+
+export function updateLineMaterial(
+  material: THREE.LineBasicMaterial | THREE.LineDashedMaterial,
+  config: LineConfig
+): THREE.LineBasicMaterial | THREE.LineDashedMaterial {
+  if (material instanceof THREE.LineBasicMaterial && config.style === LineStyle.Dotted) {
+    return updateLineMaterial(new THREE.LineDashedMaterial({ dashSize: 0.1, gapSize: 0.1 }), config);
+  }
+  if (material instanceof THREE.LineDashedMaterial && config.style === LineStyle.Solid) {
+    return updateLineMaterial(new THREE.LineBasicMaterial(), config);
+  }
+  updateMaterial(material, config.color);
+  material.linewidth = config.width;
+  return material;
 }
--- mc-rtc-ts/src/widgets/Polyhedron.ts
+++ mc-rtc-ts/src/widgets/Polyhedron.ts
@@ -1,30 +1,62 @@
 import * as THREE from 'three';
 
+import { ImGui } from '@zhobo63/imgui-ts';
+
 import { Widget } from './Widget';
 
 import { ControllerClient } from '../ControllerClient';
 
+import { Color } from '../types/Color';
 import { PolyhedronConfig } from '../types/PolyhedronConfig';
 
-import { makeMaterial, updateMaterial } from './../gui/utils';
+import { updateMaterial, updateLineMaterial } from '../gui/utils';
+
+function ColorFromImGui(label: string, color: Color) {
+  const im_color = new ImGui.Vec4(...color.data);
+  ImGui.ColorEdit4(label, im_color);
+  color.data = [im_color.x, im_color.y, im_color.z, im_color.w];
+}
 
 export class Polyhedron extends Widget {
   private visual: THREE.Mesh = null;
+  private material: THREE.MeshStandardMaterial = new THREE.MeshStandardMaterial({
+    vertexColors: true,
+    side: THREE.DoubleSide,
+    transparent: true
+  });
+  private wireframe_visual: THREE.LineSegments = null;
+  private config: PolyhedronConfig = null;
 
   constructor(client: ControllerClient, category: string[], name: string, sid: number) {
     super(client, category, name, sid);
   }
 
+  draw() {
+    if (!this.config) {
+      return;
+    }
+    if (ImGui.CollapsingHeader(this.label(`${this.name} configuration`))) {
+      ImGui.Checkbox(this.label('Show triangles'), (_ = this.config.show_triangle) => (this.config.show_triangle = _));
+      ColorFromImGui(this.label('Triangle color'), this.config.triangle_color);
+      ImGui.Checkbox(
+        this.label('Use triangle color'),
+        (_ = this.config.use_triangle_color) => (this.config.use_triangle_color = _)
+      );
+      ImGui.Checkbox('Show edges', (_ = this.config.show_edges) => (this.config.show_edges = _));
+      ColorFromImGui(this.label('Edge color'), this.config.edge_config.color);
+      ImGui.Checkbox(
+        this.label('Fix edge color'),
+        (_ = this.config.fixed_edge_color) => (this.config.fixed_edge_color = _)
+      );
+    }
+  }
+
   update(points: THREE.Vector3[], indices: number[], colors: number[], config: PolyhedronConfig) {
-    // FIXME Better handle PolyhedronConfig
     if (!this.visual) {
-      const material = new THREE.MeshStandardMaterial({
-        vertexColors: true,
-        side: THREE.DoubleSide,
-        transparent: true
-      });
-      this.visual = new THREE.Mesh(new THREE.BufferGeometry(), material);
-      this.client.gui.scene.add(this.visual);
+      this.visual = new THREE.Mesh(new THREE.BufferGeometry(), this.material);
+      this.wireframe_visual = new THREE.LineSegments();
+      this.client.gui.scene.add(this.visual, this.wireframe_visual);
+      this.config = config;
     }
     if (colors.length != 4 * points.length) {
       console.error(`Expected ${4 * points.length} data in colors but got ${colors.length}`);
@@ -34,11 +66,44 @@ export class Polyhedron extends Widget {
     this.visual.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 4));
     this.visual.geometry.setIndex(indices);
     this.visual.geometry.computeVertexNormals();
+    // FIXME Handle show_vertices
+    if (this.config.show_triangle) {
+      this.visual.visible = true;
+      if (this.config.use_triangle_color) {
+        updateMaterial(this.material, this.config.triangle_color);
+        this.material.vertexColors = false;
+      } else {
+        this.material.color = new THREE.Color(0xffffff);
+        this.material.transparent = true;
+        this.material.vertexColors = true;
+      }
+    } else {
+      this.visual.visible = false;
+    }
+    if (this.config.show_edges) {
+      this.wireframe_visual.visible = true;
+      this.wireframe_visual.geometry = new THREE.WireframeGeometry(this.visual.geometry);
+      this.wireframe_visual.geometry.setAttribute('color', this.visual.geometry.getAttribute('color'));
+      this.wireframe_visual.material = updateLineMaterial(
+        <THREE.LineBasicMaterial | THREE.LineDashedMaterial>this.wireframe_visual.material,
+        this.config.edge_config
+      );
+      (<THREE.LineBasicMaterial | THREE.LineDashedMaterial>this.wireframe_visual.material).vertexColors =
+        !this.config.fixed_edge_color;
+      if (!this.config.fixed_edge_color) {
+        (<THREE.LineBasicMaterial | THREE.LineDashedMaterial>this.wireframe_visual.material).color = new THREE.Color(
+          0xffffff
+        );
+      }
+    } else {
+      this.wireframe_visual.visible = false;
+    }
   }
 
   cleanup() {
     if (this.visual) {
       this.client.gui.scene.remove(this.visual);
+      this.client.gui.scene.remove(this.wireframe_visual);
     }
   }
 }
